5094238a4fa2a6af4271f2e35f918d9560958292
EricPHamilton <ehamilton13@gmail.com>
2018-01-06 02:33:30 -0500

    Fixed bug resulting in 'undefined' being passed as animal parameter for iframe grid url.

--- js/check_cells_per_row.js
+++ js/check_cells_per_row.js
@@ -6,7 +6,8 @@ if ($(window).width() < 500) {
    numCellsPerRow = 3;
 }
 
-var animal = document.getElementById('animal-type').name;
+var animal = document.getElementById('animal-type').getAttribute('name');
+
 var url = "https://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=" + animal + "&sex=A&agegroup=All&onhold=A&orderby=ID&colnum=" + numCellsPerRow + "&AuthKey=yoamoe7lahlatlahoe0leqoeqoaswiastouviavlatrlesplut&css=https://rawgit.com/EricPHamilton/HumaneFiles/master/css/adopt-grid.css";
 
 document.getElementById('animal-grid').src = url;
